f3400a63aaeef93c594011016b033ae52b2d6999
Steven Shih <stevenshih1997@gmail.com>
2017-03-16 17:54:30 -0700

    skeleton

--- app.js
+++ app.js
@@ -2,5 +2,6 @@
 
 var express = require('express');
 var app = express();
+var db = require('./db');
 
 module.exports = app;
---
+++ db.js
@@ -0,0 +1,2 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://server:server@ds133340.mlab.com:33340/server');
